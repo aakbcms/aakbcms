a4911c3b634d4af3339c259717e64aa166eaa1fa
Jesper Kristensen <cableman@linuxdev.dk>
2018-03-08 00:47:01 +0100

    Added pyton2

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 drush make --concurrency=1 --contrib-destination=profiles/ding2/ --working-copy drupal.make $1
 
 # Compile sass
+export PATH="/usr/local/opt/python@2/bin:$PATH"
 (cd $1/profiles/ding2/themes/ddbasic/ && npm install)
 (cd $1/profiles/ding2/themes/ddbasic/ && node_modules/.bin/gulp uglify sass)
 (cd $1/profiles/ding2/themes/ddbasic/ && rm -rf node_modules)
